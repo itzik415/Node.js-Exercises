0ac94c75a6fc0c59594e3d3795ce69209806b545
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-03-31 11:07:39 -0400

    Add Eloquent_chapter_2_exc.js exc-2

--- Eloquent_chapter_2_exc.js
+++ Eloquent_chapter_2_exc.js
@@ -6,3 +6,15 @@ while(i< 8){
 	hush+="#";
 	i++;
 }
+
+//2. FizzBuzz
+// for(let i = 1; i < 101; i++ ){
+// 	let str = '';
+// 	if(i % 3 == 0 ){
+// 		str += 'Fizz';
+// 	}
+// 	if(i % 5 == 0 ){
+// 		str += 'Buzz';
+// 	}
+// 	console.log(str || i);	
+// }
