fb9c725df9be1344fe602109d5d7de6ed951fdd5
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-04-02 11:20:25 -0400

    Add Eloquent_chapter_2_exc.js exc-3

--- Eloquent_chapter_2_exc.js
+++ Eloquent_chapter_2_exc.js
@@ -1,13 +1,13 @@
-// 1.Looping a triangle
-let hush = "#";
-let i = 1;
-while(i< 8){
-	console.log(hush)
-	hush+="#";
-	i++;
-}
+// // 1.Looping a triangle
+// let hush = "#";
+// let i = 1;
+// while(i< 8){
+// 	console.log(hush)
+// 	hush+="#";
+// 	i++;
+// }
 
-//2. FizzBuzz
+// // 2. FizzBuzz
 // for(let i = 1; i < 101; i++ ){
 // 	let str = '';
 // 	if(i % 3 == 0 ){
@@ -18,3 +18,21 @@ while(i< 8){
 // 	}
 // 	console.log(str || i);	
 // }
+
+// //3. chess board
+// let size = 8;
+
+// let board = "";
+
+// for (let y = 0; y < size; y++) {
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 == 0) {
+//       board += " ";
+//     } else {
+//       board += "#";
+//     }
+//   }
+//   board += "\n";
+// }
+
+// console.log(board);
