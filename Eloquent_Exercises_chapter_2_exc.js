f78499ac3c3ca8eccecb6cabc7a338be8b01e31b
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-04-03 09:39:58 -0400

    Add Eloquent_chapter_3_exc.js exc-1

---
+++ Eloquent_Exercises_chapter_2_exc.js
@@ -0,0 +1,38 @@
+// // 1.Looping a triangle
+// let hush = "#";
+// let i = 1;
+// while(i< 8){
+// 	console.log(hush)
+// 	hush+="#";
+// 	i++;
+// }
+
+// // 2. FizzBuzz
+// for(let i = 1; i < 101; i++ ){
+// 	let str = '';
+// 	if(i % 3 == 0 ){
+// 		str += 'Fizz';
+// 	}
+// 	if(i % 5 == 0 ){
+// 		str += 'Buzz';
+// 	}
+// 	console.log(str || i);	
+// }
+
+// //3. chess board
+// let size = 8;
+
+// let board = "";
+
+// for (let y = 0; y < size; y++) {
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 == 0) {
+//       board += " ";
+//     } else {
+//       board += "#";
+//     }
+//   }
+//   board += "\n";
+// }
+
+// console.log(board);
---
+++ Eloquent_Exercises_chapter_3_exc.js
@@ -0,0 +1,25 @@
+//1. Minimum
+// var mini = function(num1,num2){
+//     return(Math.min(num1,num2))
+// }
+
+// mini(23,21);
+
+//2. Recursion
+// var isEven = function(num){
+//     return(Boolean(num));
+// }
+
+// isEven(3);
+
+//3. Bean counting
+// var countChar = function(str,letter){
+//     var total = 0;
+//     for(var i = 0; i < str.length; i++){
+//         if(str[i] === letter){
+//             total++;
+//         }
+//     }
+// 	return total;
+// }
+// countChar("BAaaB","a");
