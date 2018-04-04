736153c56f329363c098af7968662b7e0b29a8be
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-04-04 09:56:00 -0400

    Add Eloquent_chapter_3_exc.js exc-2

--- Eloquent_Exercises_chapter_3_exc.js
+++ Eloquent_Exercises_chapter_3_exc.js
@@ -12,14 +12,4 @@
 
 // isEven(3);
 
-//3. Bean counting
-// var countChar = function(str,letter){
-//     var total = 0;
-//     for(var i = 0; i < str.length; i++){
-//         if(str[i] === letter){
-//             total++;
-//         }
-//     }
-// 	return total;
-// }
-// countChar("BAaaB","a");
+
