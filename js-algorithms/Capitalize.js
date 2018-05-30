1771a3c0f9b83da275a0ea2ecf48e7de668d6707
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-05-30 00:16:09 -0400

    Capitalize.js solution-2 add + improve solution-1

--- js-algorithms/Capitalize.js
+++ js-algorithms/Capitalize.js
@@ -2,12 +2,27 @@
 var capitalize = function(str){
 	let array = str.toLowerCase().split(' ');
 	let newArray = [];
-	for(let i = 0; i < array.length; i++) {
-		newArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1));
+	for(let word of array) {
+		newArray.push(word.charAt(0).toUpperCase() + word.slice(1));
 	}
 	return newArray.join(' ');
 }
 
 capitalize('a short string');
 
+//Solution 2
+let capitalize = (str) => {
+    let result = str[0].toUpperCase();
 
+    for (let i = 1; i < str.length; i++) {
+        if (str[i-1] === ' ') {
+            result += str[i].toUpperCase();
+        }else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+capitalize('a short string');
