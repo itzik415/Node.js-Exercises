420859758674e78ec8c6117d72299263a3e351e9
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-06-02 16:14:10 -0400

    steps.js solution2 +3 add

--- js-algorithms/steps.js
+++ js-algorithms/steps.js
@@ -6,9 +6,45 @@ const steps = function(N){
         console.log('');
     }else {
         for(let i = 1; i <= N; i++){
-            console.log( hash.repeat(i) + space.repeat(N-i))    
+            console.log("'" + hash.repeat(i) + space.repeat(N-i) + "'")    
         }
     }
+}
+
+steps(4);
+
+//Solution2
+const steps = function(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log("'" + stair + "'");
+    }
+}   
+
+steps(4);
+
+//Solution3
+const steps = function(n, row = 0, stair = '') {
+    if (n == row) {
+        return;
+    }  
+
+    if (n === stair.length){
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair);
 }
 
 steps(4);
