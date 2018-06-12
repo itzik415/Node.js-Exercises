8ef87ecffcca1abe29e3798446a3953804c2b0d1
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-06-12 14:18:34 -0400

    chunk.js solution1 add

---
+++ js-algorithms/chunk.js
@@ -0,0 +1,16 @@
+//Solution 1
+var chunk = function(array, size){
+    let array2 = [];
+    for (let element of array) {
+        const last = array2[array2.length - 1];
+        
+        if(!last || last.length === size) {
+            array2.push([element])
+        } else {
+            last.push(element)
+        }
+    }
+    return array2;
+}
+
+chunk([1,2,3,4], 2);
