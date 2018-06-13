8f2d13c6a1902adbc1fcf1c0da09c517b75a2f20
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-06-13 14:19:16 -0400

    chunk.js solution2 add

--- js-algorithms/chunk.js
+++ js-algorithms/chunk.js
@@ -14,3 +14,18 @@ var chunk = function(array, size){
 }
 
 chunk([1,2,3,4], 2);
+
+//Solution 2
+var chunk = function(array, size){
+    let array2 = [];
+    let index = 0;
+
+    while (index < array.length) {
+        array2.push(array.slice(index, index + size))
+        index += size;
+    }
+
+    return array2;
+}
+
+chunk([1,2,3,4], 3);
