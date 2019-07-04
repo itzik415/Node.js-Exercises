382c67a8da5f1b893a416c18f5609a3efb0c1c9a
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-07-04 00:39:27 -0400

    findDuplicateFiles.js second solution

--- js-algorithms/findDuplicateFiles.js
+++ js-algorithms/findDuplicateFiles.js
@@ -61,3 +61,15 @@ var findDuplicate = function(paths) {
 findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"])
 
    
+// var findDuplicate = function(paths) {
+//     const map = {};
+//     for (let text of paths) {
+//         for (let i = 1, files = text.split(' '); i < files.length; i++) {
+//             let paren = files[i].indexOf('(');
+//             let content = files[i].substring(paren + 1, files[i].length - 1);
+//             map[content] = map[content] || [];
+//             map[content].push(files[0] + '/' + files[i].substr(0, paren));
+//         }
+//     }
+//     return Object.values(map).filter(dups => dups.length > 1);
+// };
